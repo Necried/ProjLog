dc529aa3e9b73ba4b43856eec9b516c4af5e9bbd
Necried <duttonl@mcmaster.ca>
2018-02-27 14:03:25 -0500

    minor changes, needs more implementation

--- main.hs
+++ main.hs
@@ -6,11 +6,26 @@ import System.Directory
 
 instance Node Server
 
+-- A log entry data-type
+data LogEntry = LogEntry { Date :: Int,
+                           Originator :: String,
+                           Type :: String,
+                           Status :: String,
+                           Comments :: String,
+                           Doc :: FilePath
+                         }
+
+-- A log header
+data LogHeader = LogHeader { ProjName :: String,
+                             Author :: String,
+                             Members :: [String]
+                           }
+                           
 -- `appendChildren parent children` adds a list of children to a parent element
 appendChildren :: Elem -> [Elem] -> Client ()
 appendChildren parent children = sequence_ [appendChild parent c | c <- children]
 
--- Server sanity-check function
+-- Server sanity-check function: Prints to CLI
 greet :: RemotePtr (String -> Server ())
 greet = static (native $ remote $ liftIO . putStrLn)
 
@@ -18,39 +33,67 @@ greet = static (native $ remote $ liftIO . putStrLn)
 getFiles :: RemotePtr (String -> Server [FilePath])
 getFiles = static (remote $ liftIO . listDirectory)
 
+-- File name extension slicing
+sliceExt :: FilePath -> String
+sliceExt filename = takeWhile (/='.') filename
+
+-- sanitize input filepaths server-side to prevent malicious access
+sanitizeFilepath :: RemotePtr (FilePath -> Server FilePath)
+sanitizeFilepath = static (remote $ return . sanitize)
+
+-- Helper sanitize function
+sanitize :: FilePath -> FilePath
+sanitize [] = []
+sanitize (x:[]) = if x == '.' then "?" else x:[] 
+sanitize (x:y:xs) = if (x == '.' && y == '.') then "?" else x:(sanitize (y:xs))
+
 -- Put files retrieved from the server into the browser
 putFiles :: [FilePath] -> Client ()
-putFiles [] = do
-  withElem "list" $ \list -> do
-    submit <- newElem "input" `with` [ attr "type" =: "submit",
-                                       attr "name" =: "Submit",
-                                       attr "value" =: "Edit selected file" ]
-    download <- newElem "input" `with` [ attr "type" =: "submit",
-                                       attr "name" =: "Download",
-                                       attr "value" =: "Download selected file" ]
-    break <- newElem "br"
-    appendChildren list [break, submit, download]
+putFiles [] = return ()
 putFiles (file:filelist) = do
   withElem "list" $ \list -> do
-    identifier <- newElem "span" `with` [ prop "innerHTML" =: file ]
-    radio <- newElem "input" `with` [ attr "type"  =: "radio",
-                                         attr "value" =: file,
-                                         attr "name"  =: "filetype",
-                                         prop "innerHTML" =: file ]
-    break <- newElem "br"
-    appendChildren list [radio, identifier, break]
+    li <- newElem "li" `with` [ attr "class" =: "list-item", prop "innerHTML" =: (sliceExt file ++ "   ") ]
+    add <- newElem "a" `with` [ attr "href" =: "#", prop "innerHTML" =: "Add" ]
+    spacing <- newElem "span" `with` [ prop "innerHTML" =: "     " ]
+    -- Remember to change this to a .pdf relative file path once features are implemented!
+    dl <- newElem "a" `with` [ attr "href" =: ("./data/" ++ file) , prop "innerHTML" =: "Download" ]
+    appendChild list li
+    appendChildren li [add, spacing, dl]
     putFiles filelist
 
+-- Takes a filename, and creates a log entry form
+-- appendForm :: String -> Client ()
+
+-- Takes a filename, a list of log headers, and creates a new latex project log file
+-- This is (obviously) done server-side. Also, pdflatex should be run
+-- after appending (temporarily for now, as this is not so efficient)
+-- createLog :: RemotePtr (String -> [String] -> Server ()) 
+
+-- Takes a filepath, a list of log entries, and appends to the latex project log file
+-- Also done server-side
+-- appendLog :: RemotePtr (FilePath -> [String] -> Server ())
+
+-- Run the application
 main = runApp [start (Proxy :: Proxy Server)] $ do
-  -- fetch stored tex files
-  files <- dispatch getFiles "./data"
+  -- fetch stored tex files. Sanitize the input first!
+  name <- dispatch sanitizeFilepath "../ExpenseApp"
+  files <- dispatch getFiles name
   putFiles files
-  withElem "Download" $ \dl -> do
-    dl `onEvent` Click $ \_ -> do
-      filename <- getValue dl "value"
-      dispatch greet filename
   withElem "btn" $ \btn -> do
     btn `onEvent` Click $ \_ -> do
       alert (toJSString $ "ok")
+      
   return ()
+{-
+main :: IO ()
+main = do
+  ul <- elemsByQS document "ul#demo-list"
+  case ul of
+    (el:_) -> mapQS_ document "#demo-list li" (handleRemove el)
+    _      -> error "Element 'ul#demo-list' not found"
 
+handleRemove :: Elem -> Elem -> IO HandlerInfo
+handleRemove ul li = do
+  onEvent li Click $ \_ -> do
+    deleteChild ul li
+-}
